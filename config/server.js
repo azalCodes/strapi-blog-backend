cea586fa98f8c3f60f10edb3003d064963d8dec2
Azal Abedi <azal.abedi@gmail.com>
2021-09-15 09:51:33 -0600

    initial backend quickstart Strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9d64f3990ba9164a40cf224fc16cbb7d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8c716094-9306-4710-8990-0c2f701fe29c'
+};
